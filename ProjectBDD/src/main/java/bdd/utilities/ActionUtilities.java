537e6841dd3b900dc052992d38bd3b7c5922a5c7
ajaysoma999 <somaajay999@gmail.com>
2024-04-27 15:36:53 +0530

    Add the comments for the two action methods

--- ProjectBDD/src/main/java/bdd/utilities/ActionUtilities.java
+++ ProjectBDD/src/main/java/bdd/utilities/ActionUtilities.java
@@ -6,14 +6,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
+import java.time.D
 public class ActionUtilities {
     WebDriver driver;
     public ActionUtilities(WebDriver driver) {
         this.driver = driver;
 
     }
+   /*
+   waitForElement :
+   Wait operation for webelement by using explicet wait
+   With provided durationInSeconds
+   */
     public WebElement waitForElement(WebElement element,long durationInSeconds) {
         WebElement webElement = null;
         try {
@@ -26,14 +30,23 @@ public class ActionUtilities {
         return webElement;
 
     }
-
-
-    public void selectOptionInDropdown(WebElement element, String dropDownOption, long durationInSeconds) {
+    /*
+    selectOptionInDropdown:
+    select the the dropdown field by selectByVisibleText method
+    and also waitForElement is applyed
+    */
+    public void DropDown(WebElement element, String dropDownOption, long durationInSeconds) {
 
         WebElement webElement = waitForElement(element,durationInSeconds);
         Select select = new Select(webElement);
         select.selectByVisibleText(dropDownOption);
 
     }
+
+   public void Click(WebElement element){
+
+
+   }
+
 
 }
